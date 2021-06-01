0f192aa3c5990521af21608790598c97bb625cd8
13jk59 <jesse.khaira10@gmail.com>
2021-06-01 13:16:43 -0600

    added pylintrc file, yapf file, and cleaned up decoder

--- model/Decoder.py
+++ model/Decoder.py
@@ -1,7 +1,7 @@
 from model.Embedding_layer import Embedding_layer
 from model.RNN_cell import RNN_cell
 from model.Utils import crossEntropy
-from collections import deque 
+
 
 class Decoder(object):
     """
@@ -16,13 +16,21 @@ class Decoder(object):
         -> dim_embed_trg (int): Size of the embeddings for the decoder
         -> optim (class): Optimizer used to train the parameters within the model
         -> num_neurons_decoder (int): Number of neurons in the decoder 
-    """ 
-    def __init__(self, vocab_size_trg, dim_embed_trg, num_neurons_decoder, optim):
-        self.embedding_layer = Embedding_layer(vocab_size_trg, dim_embed_trg, optim)
+    """
+
+    def __init__(self, vocab_size_trg, dim_embed_trg, num_neurons_decoder,
+                 optim):
+        self.embedding_layer = Embedding_layer(vocab_size_trg, dim_embed_trg,
+                                               optim)
         # for the decoder, we're going to tie the weights of the embedding layer and the linear projection
         # before softmax activation. If vocab_size_src and vocab_size_trg are same as well, its possible to tie all
         # the weights but not done here for simplicity of implementation . See: https://arxiv.org/abs/1608.05859
-        self.rnn_cell = RNN_cell(dim_embed_trg, num_neurons_decoder, optim, self.embedding_layer, predict = True, costFunction = crossEntropy) 
+        self.rnn_cell = RNN_cell(dim_embed_trg,
+                                 num_neurons_decoder,
+                                 optim,
+                                 self.embedding_layer,
+                                 predict=True,
+                                 costFunction=crossEntropy)
 
     def __call__(self, encoded_batch, target_language_seqs, mask):
         """
@@ -49,19 +57,22 @@ class Decoder(object):
         Outputs:
             -> integer representing the loss on this batch
         
-        """ 
+        """
         # Shape (M, T-1)
         x_matrix = target_language_seqs[:, :-1]
-        y_matrix = target_language_seqs[:,1:]
-        _, loss, _ = self.rnn_cell._forward(x=x_matrix, y =y_matrix, a_prev=encoded_batch, mask = mask)
-        return loss 
+        y_matrix = target_language_seqs[:, 1:]
+        _, loss, _ = self.rnn_cell._forward(x=x_matrix,
+                                            y=y_matrix,
+                                            a_prev=encoded_batch,
+                                            mask=mask)
+        return loss
 
     def _backward(self, learn_rate):
-        dencoded_batch= self.rnn_cell._backward(learn_rate=learn_rate)
+        dencoded_batch = self.rnn_cell._backward(learn_rate=learn_rate)
         return dencoded_batch
 
-    def beamSearch(self, encoded, eos_int, sos_int, length_normalization, beam_width, max_seq_len):
-        return self.rnn_cell._beamSearch(encoded, eos_int, sos_int, length_normalization, beam_width, max_seq_len)
-    
-
-
+    def beamSearch(self, encoded, eos_int, sos_int, length_normalization,
+                   beam_width, max_seq_len):
+        return self.rnn_cell._beamSearch(encoded, eos_int, sos_int,
+                                         length_normalization, beam_width,
+                                         max_seq_len)
